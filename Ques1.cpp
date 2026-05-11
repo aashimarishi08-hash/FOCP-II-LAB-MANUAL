ecd27e0ef3c7a964b6cfab892065030ec3d51d27
aashimarishi08-hash <aashimarishi08@gmail.com>
2026-05-11 14:50:16 +0530

    Create Ques1.cpp

---
+++ Ques1.cpp
@@ -0,0 +1,10 @@
+Q1. Average of Three Students' Marks
+#include <iostream>
+using namespace std;
+int main() {
+    float a, b, c;
+    cout << "Enter marks of 3 students: ";
+    cin >> a >> b >> c;
+    cout << "Average: " << (a + b + c) / 3 << endl;
+    return 0;
+}
