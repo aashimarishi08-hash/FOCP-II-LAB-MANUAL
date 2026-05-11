8951d5632de8564f1115c4bbeebafb368fc03e1a
aashimarishi08-hash <aashimarishi08@gmail.com>
2026-05-11 14:50:57 +0530

    Create Ques2.cpp

---
+++ Ques2.cpp
@@ -0,0 +1,11 @@
+Q2. Area of a Circle
+#include <iostream>
+using namespace std;
+int main() {
+    float r;
+    const float PI = 3.14159;
+    cout << "Enter radius: ";
+    cin >> r;
+    cout << "Area: " << PI * r * r << endl;
+    return 0;
+}
