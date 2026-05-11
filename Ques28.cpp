25acbd65407275c2c3770d55f0ebed3935519781
aashimarishi08-hash <aashimarishi08@gmail.com>
2026-05-11 15:03:31 +0530

    Create Ques28.cpp

---
+++ Ques28.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[5], evenSum = 0, oddSum = 0;
+
+    cout << "Enter 5 numbers:" << endl;
+
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+
+        if(arr[i] % 2 == 0)
+            evenSum += arr[i];
+        else
+            oddSum += arr[i];
+    }
+
+    cout << "Sum of Even Numbers = " << evenSum << endl;
+    cout << "Sum of Odd Numbers = " << oddSum;
+
+    return 0;
+}
